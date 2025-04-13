feee2681621539f21ad31390c339322bc216e9b2
andrasd2004 <andrasd2004@gmail.com>
2025-04-13 16:04:12 +0200

    number pipe, 404 javítás

--- web_app/src/app/shared/page-not-found/page-not-found.component.ts
+++ web_app/src/app/shared/page-not-found/page-not-found.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
 
 
 @Component({
   selector: 'app-page-not-found',
   imports: [
-    RouterLink
+    RouterLink,
+    MatButtonModule
   ],
   templateUrl: './page-not-found.component.html',
   styleUrl: './page-not-found.component.scss'
